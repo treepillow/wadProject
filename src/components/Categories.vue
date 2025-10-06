c568763e878187d0019e455d8a568832dc3f6792
twilightdream <quekaaron25@gmail.com>
2025-10-07 01:00:17 +0800

    Navigation bar for main page

---
+++ src/components/Categories.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+
+
+
+<template>
+<div class="container-fluid">
+  <nav class="navbar">
+
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/fooddrinks.jpg" alt="FoodandDrinks">
+            <div class="category-text fs-2 text-center fw-normal">Food & Drinks</div>
+        </a>
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/beauty.png" alt="Beauty" >
+            <div class="category-text fs-2 text-center fw-normal">Beauty</div>
+        </a>
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/fitness.jpg" alt="FitnessAndWellness" >
+            <div class="category-text fs-2 text-center fw-normal">Fitness</div>
+        </a>
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/arts.jpg" alt="ArtsAndCraft">
+            <div class="category-text fs-2 text-center fw-normal">Arts & Craft</div>
+        </a>
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/education.jpg" alt="Education">
+            <div class="category-text fs-2 text-center fw-normal">Education</div>
+        </a>
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/pets.png" alt="Pets">
+            <div class="category-text fs-2 text-center fw-normal">Pets</div>
+        </a>
+        <a class="navbar-brand category" href="#">
+            <img src="../assets/others.png" alt="Others">
+            <div class="category-text fs-2 text-center fw-normal">Others</div>
+        </a>
+</nav>
+
+<hr></hr>
+</div>
+
+
+
+</template>
+
+
+
+
+
+<style scoped>
+    hr {
+        border: 1;
+        opacity: 0.25;
+    }
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,93 @@
+<script setup>
+
+
+</script>
+
+<template>
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg">
+
+    <a class="navbar-brand" href="#">Homes</a>
+
+
+    <div class="d-flex ms-auto">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                    <img src="../assets/message_icon.png"></img>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link profile-icon" href="#">
+                    <img src="../assets/user.png"></img>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    </nav>
+  <div class ="search mt-2">
+    <form class="d-flex" role="search">
+      <input class="home-business form-control me-3" type="search" placeholder="🔍 Search for a home business" aria-label="Search"/>
+        <input class="location-search form-control me-3" type="search" placeholder="📍 All of Singapore" aria-label="Search"/>   
+        <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+
+  </div>
+</div>
+
+</template>
+
+
+
+<style>
+    .navbar{
+        background-color: rgb(243, 233, 243); /* Set background color */
+
+    }
+
+    .navbar-brand
+    {
+        font-size: 50px;
+        font-weight: bold;
+        color: purple;
+    }
+
+    .profile-icon
+    {
+        border: 3px solid black;
+        border-radius: 50px;
+    }
+
+    .nav-item img
+    {
+        width: 50px;
+    }
+
+    .home-business
+    {
+        width: 70%;
+        padding: 15px;
+    }
+    .location-search
+    {
+        width: 20%;
+    }
+    .btn
+    {
+        width: 10%;
+    }
+
+    .category img
+    {
+        width: 200px;
+        height: 200px;
+        border: 1px solid black;
+        border-radius: 100px;
+    }
+
+
+    
+</style>
